dd34530ddff7f99d660719aae35aee88714560e2
Ramsay Taylor <ramsay@gmail.com>
2013-01-22 13:50:53 +0000

    Allow specification of certain functions to instrument.

--- src/mu2_logging_mutations.erl
+++ src/mu2_logging_mutations.erl
@@ -12,13 +12,32 @@ log_mutation(File) ->
 		       true)], 
 		[File]).
 
+specific_log_mutation(File, FunctionNames) ->
+    ?FULL_TD_TP([?RULE(?T("f@(Args@@) when Guard@@-> Body@@;"), 
+		       begin
+			   {NewArgs@@, _} = convert_args(Args@@, 1),
+			   ?TO_AST("f@(NewArgs@@) when Guard@@-> mu2_logging_mutations:log(?MODULE, f@, [NewArgs@@]), Body@@;")
+		       end,
+		       contains(FunctionNames, list_to_atom(?PP(f@))))], 
+		[File]).
+
 apply_logging([FileAtom, OutFileAtom]) ->
     File = atom_to_list(FileAtom),
     OutFile = atom_to_list(OutFileAtom),
     io:format("Applying all mutations to ~p and writing to ~p...~n", [File, OutFile]),
     wrangler_ast_server:start_ast_server(),
     {ok, [{{File, File}, ST}]} = log_mutation(File),
+    mu2_output:write_mutant([], OutFile, {File, "Logging", "all", "all", ST});
+
+apply_logging([FileAtom, OutFileAtom | FunctionNames]) ->
+    File = atom_to_list(FileAtom),
+    OutFile = atom_to_list(OutFileAtom),
+    io:format("Applying mutations to ~p in ~p and writing to ~p...~n", [FunctionNames, File, OutFile]),
+    wrangler_ast_server:start_ast_server(),
+    {ok, [{{File, File}, ST}]} = specific_log_mutation(File, FunctionNames),
     mu2_output:write_mutant([], OutFile, {File, "Logging", "all", "all", ST}).
+
+
 
 log(M, F, A) ->
     mu2_logger:log(M,F,A).
@@ -58,3 +77,11 @@ convert_if_nec(Arg, Next) ->
     %% Some other type of argument - maybe an atom?
     io:format("Unhandled arg... ~p~n", [Arg]),
     {Arg, Next}.
+
+contains([], _) ->
+    false;
+contains([L |_], L) ->
+    true;
+contains([_ | Ls], L) ->
+    contains(Ls, L).
+
