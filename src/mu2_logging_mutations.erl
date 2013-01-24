d9e7e4ea19755b62ac15d00732b37ce737f05bfe
Ramsay Taylor <ramsay@gmail.com>
2013-01-24 14:03:19 +0000

    Various instrumentation work
    * Instrumentation now handles all argument types. Non-trivial arguments
    are placed in a match_expr with a new variable and that variable is
    used for logging
    * Log_id parameter added to logging, which allows multiple modules to
    log to the same file.
    * Log parsing functions allow combined log to be broken up into traces.
    When given a module name it finds sequences of calls to that module and
    makes traces. This works best when logging a child module and its
    parent, the child module traces are then broken up by calls to the
    parent module (e.g. {m_test, f, 1} {m,a,1} {m,b,2} {m_test, g, 2}
    {m,a,2} etc.).

--- src/mu2_logging_mutations.erl
+++ src/mu2_logging_mutations.erl
@@ -1,62 +1,61 @@
 -module(mu2_logging_mutations).
--export([log_mutation/1, apply_logging/1, apply_relevant_logging/1, log/3]).
+-export([apply_logging/1, apply_relevant_logging/1]).
 
 -include("../include/mutations.hrl").
 
-log_mutation(File) ->
+log_mutation(File, LogID) ->
     ?FULL_TD_TP([?RULE(?T("f@(Args@@) when Guard@@-> Body@@;"), 
 		       begin
 			   {NewArgs@@, LogArgs@@, _} = convert_args(Args@@, 1),
-			   ?TO_AST("f@(NewArgs@@) when Guard@@-> mu2_logging_mutations:log(?MODULE, f@, [LogArgs@@]), Body@@;")
+			   ?TO_AST("f@(NewArgs@@) when Guard@@-> mu2_logger:log(?MODULE, f@, [LogArgs@@], " ++ LogID ++ "), Body@@;")
 		       end,
 		       true)], 
 		[File]).
 
-specific_log_mutation(File, FunctionNames) ->
+specific_log_mutation(File, FunctionNames, LogID) ->
     ?FULL_TD_TP([?RULE(?T("f@(Args@@) when Guard@@-> Body@@;"), 
 		       begin
 			   {NewArgs@@, LogArgs@@, _} = convert_args(Args@@, 1),
-			   ?TO_AST("f@(NewArgs@@) when Guard@@-> mu2_logging_mutations:log(?MODULE, f@, [LogArgs@@]), Body@@;")
+			   ?TO_AST("f@(NewArgs@@) when Guard@@-> mu2_logger:log(?MODULE, f@, [LogArgs@@], " ++ LogID ++ "), Body@@;")
 		       end,
 		       contains(FunctionNames, {list_to_atom(?PP(f@)), length(Args@@)}))], 
 		[File]).
 
 apply_logging([FileAtom, OutFileAtom]) ->
-    File = atom_to_list(FileAtom),
-    OutFile = atom_to_list(OutFileAtom),
-    io:format("Applying all mutations to ~p and writing to ~p...~n", [File, OutFile]),
-    wrangler_ast_server:start_ast_server(),
-    {ok, [{{File, File}, ST}]} = log_mutation(File),
-    mu2_output:write_mutant([], OutFile, {File, "Logging", "all", "all", ST});
+    io:format("Applying logging to all functions.~n"),
+    apply_logging(FileAtom, OutFileAtom, fun(File, LogID) -> log_mutation(File, LogID) end);
 
 apply_logging([FileAtom, OutFileAtom | FunctionNames]) ->
-    File = atom_to_list(FileAtom),
-    OutFile = atom_to_list(OutFileAtom),
-    io:format("Applying mutations to ~p in ~p and writing to ~p...~n", [FunctionNames, File, OutFile]),
-    wrangler_ast_server:start_ast_server(),
-    {ok, [{{File, File}, ST}]} = specific_log_mutation(File, FunctionNames),
-    mu2_output:write_mutant([], OutFile, {File, "Logging", "all", "all", ST}).
+    io:format("Applying logging to ~p.~n", [FunctionNames]),
+    apply_logging(FileAtom, OutFileAtom, fun(File, LogID) -> specific_log_mutation(File, FunctionNames, LogID) end).
 
 %% Attempts to determine a suitable external alphabet and instrument only those functions
 apply_relevant_logging([FileAtom, OutFileAtom]) ->
+    Module = mu2_output:get_module_name_from_filename(atom_to_list(FileAtom)),
+    FunctionNames = mu2_module_explorer:get_alphabet(list_to_atom(Module)),
+    io:format("Applying logging to ~p.~n", [FunctionNames]),
+    apply_logging(FileAtom, OutFileAtom, fun(File, LogID) -> specific_log_mutation(File, FunctionNames, LogID) end).
+
+apply_logging(FileAtom, OutFileAtom, Mutation) ->
     File = atom_to_list(FileAtom),
     OutFile = atom_to_list(OutFileAtom),
-    Module = mu2_output:get_module_name_from_filename(File),
-    FunctionNames = mu2_module_explorer:get_alphabet(list_to_atom(Module)),
-    io:format("Applying mutations to ~p in ~p and writing to ~p...~n", [FunctionNames, File, OutFile]),
+    LogID = case init:get_argument(log_id) of
+		{ok, Val} ->
+		    io:format("Will write log to ~p~n", [hd(hd(Val))]),
+		    hd(hd(Val));
+		_NotPresent -> 
+		    mu2_output:get_module_name_from_filename(File)
+	    end,
+    io:format("Applying logging mutations to ~p and writing to ~p...~n", [File, OutFile]),
     wrangler_ast_server:start_ast_server(),
-    {ok, [{{File, File}, ST}]} = specific_log_mutation(File, FunctionNames),
+    {ok, [{{File, File}, ST}]} = Mutation(File, LogID),
     mu2_output:write_mutant([], OutFile, {File, "Logging", "all", "all", ST}).
     
-log(M, F, A) ->
-    mu2_logger:log(M,F,A).
-
 convert_args([], Next) ->
     {[], [], Next};
 convert_args([A | AS], Next) ->
     {Arg, LogArg, NewNext} = convert_if_nec(A, Next),
     {NewArgs, NewLogArgs, NewNewNext} = convert_args(AS, NewNext),
-    %%io:format("Converting~n~p~n---------~n~p~n>>>>>>>>>>>~n~p~n~n", [A, [Arg|NewArgs], [LogArg|NewLogArgs]]),
     {[Arg | NewArgs], [LogArg | NewLogArgs], NewNewNext};
 convert_args(Wibble, Next) ->
     io:format("Can't convert this list of args: ~p~n", [Wibble]),
@@ -80,39 +79,21 @@ convert_if_nec({wrapper,variable,{attr, X, Attrs, Y}, {var,Loc,Name}}, Next)  ->
 	     {wrapper,variable,{attr, X, Attrs, Y}, {var,Loc,Name}},
 	      Next}
     end;
-convert_if_nec({tree, list, Attrs, {list, Content, Last}}, Next) ->
-    {NewContent, NewLogContent, NewNext} = convert_args(Content, Next),
-    {NewLast, NewNewNext} = convert_if_nec(Last, NewNext),
-    {{tree, list, Attrs, {list, NewContent, NewLast}}, 
-     {tree, list, Attrs, {list, NewLogContent, NewLast}}, 
-     NewNewNext};
-convert_if_nec({tree, match_expr, Attrs, {match_expr, Left, Right}}, Next) ->
-%%    io:format(">>>>>>>>>>>>>>>>>>~n~p~n~n~p~n<<<<<<<<<<<<<<<<<<<<<<~n", [Left,Right]),
-%%erlang:error("Arg...."),
-    {NewRight, NewLogRight, NewNext} = convert_if_nec(Right, Next),
-    {{tree, match_expr, Attrs, {match_expr, Left, NewRight}}, 
-     NewLogRight, 
-     NewNext};
-convert_if_nec({tree, Type, Attrs, Content}, Next) ->
-    {NewContent, NewLogContent, NewNext} = convert_args(Content, Next),
-    {{tree, Type, Attrs, NewContent}, 
-     {tree, Type, Attrs, NewLogContent}, 
-     NewNext};
 convert_if_nec({wrapper, atom, Attrs, Content}, Next) ->
     %% nothing to do to atoms...
     {{wrapper, atom, Attrs, Content}, 
      {wrapper, atom, Attrs, Content}, 
      Next};
-convert_if_nec(Arg, Next) ->
-    %% Some other type of argument - maybe an atom?
-    io:format("Unhandled arg... ~p~n", [Arg]),
-    {Arg, Arg, Next}.
+convert_if_nec({_Name, _Type, {attr, Loc, _AttrContent, _Something} = Attr, _Content} = Arg, Next) ->
+    Right = {wrapper, variable, Attr, {var,Loc,list_to_atom("MU2_LOG_" ++ lists:flatten(io_lib:format("~p", [Next])))}},
+    {{tree, match_expr, Attr, {match_expr, Arg, Right}}, Right, Next+1};
+convert_if_nec(Arg, _Next) ->
+    io:format("Failed to handle:~n~p~n", [Arg]),
+    erlang:error("Failed to handle an arg.").
 
-contains([], L) ->
-    io:format("Doesn't contain ~p~n", [L]),
+contains([], _L) ->
     false;
 contains([L |_], L) ->
-    io:format("Contains ~p~n", [L]),
     true;
 contains([_ | Ls], L) ->
     contains(Ls, L).
