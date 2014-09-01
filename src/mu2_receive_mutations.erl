e758828cd802a2d6047dae10d54c76781b641996
Ramsay Taylor <r.taylor@dcs.shef.ac.uk>
2014-09-01 08:52:01 +0100

    Added increase and decrease timeout mutations.

--- src/mu2_receive_mutations.erl
+++ src/mu2_receive_mutations.erl
@@ -52,19 +52,25 @@ receive_mutations() ->
     		    NewBody@@@ = lists:sublist(Body@@@, length(Pats@@@) -1),
 		    ?TO_AST("receive NewPats@@@ when NewGuards@@@ -> NewBody@@@ end")
     		end)}
-%% ,
-%%     {increase_timeout,
-%%       ?MUTATION_RESTRICT("receive Pats@@@ when Guards@@@ -> Body@@@ after APats@@@ -> ABody@@@ end",
-%% 			 begin
-%% 			     io:format("got Timeout: ~p~n",[APats@@@]),
-%% 			     length(APats@@@) > 0
-%% 			 end),
-%%       ?MUTATION("receive Pats@@@ when Guards@@@ -> Body@@@ after APats@@@ -> ABody@@@ end",
-%%     		begin
-%%     		    NewPats@@@ = lists:sublist(Pats@@@, length(Pats@@@) -1),
-%%     		    NewGuards@@@ = lists:sublist(Guards@@@, length(Pats@@@) -1),
-%%     		    NewBody@@@ = lists:sublist(Body@@@, length(Pats@@@) -1),
-%% 		    ?TO_AST("receive NewPats@@@ when NewGuards@@@ -> NewBody@@@ end")
-%%     		end)}
+    ,{increase_timeout,
+      ?MUTATION_MATCH("receive Pats@@@ when Guards@@@ -> Body@@@ after APats@@@ -> ABody@@@ end"),
+      ?MUTATION("receive Pats@@@ when Guards@@@ -> Body@@@ after APats@@ -> ABody@@ end",
+    		begin
+		    NewAPats@@ = lists:map(fun(Pat@) -> 
+		    				    ?TO_AST("(Pat@ + 10000)")  
+		    			    end,
+		    			    APats@@),
+		    ?TO_AST("receive Pats@@@ when Guards@@@ -> Body@@@ after NewAPats@@ -> ABody@@ end")
+    		end)}
+    ,{decrease_timeout,
+      ?MUTATION_MATCH("receive Pats@@@ when Guards@@@ -> Body@@@ after APats@@@ -> ABody@@@ end"),
+      ?MUTATION("receive Pats@@@ when Guards@@@ -> Body@@@ after APats@@ -> ABody@@ end",
+    		begin
+		    NewAPats@@ = lists:map(fun(Pat@) -> 
+		    				    ?TO_AST("(Pat@ / 100)")  
+		    			    end,
+		    			    APats@@),
+		    ?TO_AST("receive Pats@@@ when Guards@@@ -> Body@@@ after NewAPats@@ -> ABody@@ end")
+    		end)}
     ].
 
